85389ab4f8e6e7c45f5b1ffaa13f68afee0e7497
Igor Padoim <igorpadoim@gmail.com>
2026-01-07 21:52:25 -0300

    Fix class reference

--- lib/ruby_crawler.rb
+++ lib/ruby_crawler.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'ruby_crawler/version'
+require_relative 'ruby_crawler/meta'
 
+# Main module for RubyCrawler
 module RubyCrawler
-  class Error < StandardError; end
-  # Your code goes here...
 end
